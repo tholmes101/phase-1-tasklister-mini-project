dfc33c9404b578d12e1c231d06692fe97b5ddda8
Terence Holmes <piece4ulmind@gmail.com>
2022-05-04 17:12:40 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  //const task = document.getElementById("new-task-description")
+  const form = document.querySelector("form")
+  const tasks = document.querySelector("#tasks")
+
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const inputValue = e.target.task.value
+    addElement(inputValue)
+  })
+
+
+function addElement(element) {
+  let p = document.createElement("p")
+  p.textContent = ` ${element} `
+  tasks.appendChild(p)
+}
+
 });
+
+
